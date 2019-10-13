714662f0947df0e58c435e891380e743870c62a3
WillianBermudezNavarro <willian.cheveres@gmail.com>
2019-10-12 21:47:57 -0500

    Poligono

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 # include <OpenGL/gl.h>
 # include <OpenGL/glu.h>
 # include <GLUT/glut.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #else
 # include <GL/gl.h>
 # include <GL/glu.h>
 # include <GL/glut.h>
 #endif
+#include <windows.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+#define maxWD 800
+#define maxHT 600
+
+vector<int> coor1, coor2;
 
 GLsizei winWidth =800, winHeight =600;
 int x1=-1,y1=-1,x2=-1,y2=-1;
@@ -26,9 +33,9 @@ void init()
 }
 void displayFcn(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT); 
 	glColor3f(1.0,0.0,0.0); 
-	glPointSize(1.0);
+	glPointSize(2.0); 
 }
 
 void winReshapeFcn(GLint newWidth, GLint newHeight)
@@ -49,70 +56,81 @@ void plotPoint(GLint x, GLint y)
 	glEnd();
 }
 
-inline int round (const float a) { return int (a + 0.5); }
-void linea (int x0, int y0, int xEnd, int yEnd)
+int round(int a) 
+{ return float (a + 0.5); }
+void linea(int x0, int y0, int xEnd, int yEnd)
 {
-	int dx = xEnd - x0, dy = yEnd - y0, pasos, k;
-	float xp, yp, x = x0, y = y0;
-	if (abs (dx) > abs (dy))
-		pasos = abs (dx);
+	int dx = xEnd - x0, dy = yEnd - y0, steps, k;
+	float xIncrement, yIncrement, x = x0, y = y0;
+	if (fabs (dx) > fabs (dy))
+		steps = fabs (dx);
 	else
-		pasos = abs (dy);
-	xp = float (dx) / float (pasos);
-	yp = float (dy) / float (pasos);
+		steps = fabs (dy);
+	xIncrement = float (dx) / float (steps);
+	yIncrement = float (dy) / float (steps);
 	plotPoint(round (x), round (y));
-	for (k = 0; k < pasos; k++)
+	for (k = 0; k < steps; k++)
 	{
-		x += xp;
-		y += yp;
+		x += xIncrement;
+		y += yIncrement;
 		plotPoint(round (x), round (y));
 	}
+}
+void poligono()
+{
+	int tam=coor1.size();
+	for(int i=1; i<tam; i++)
+	{
+		linea(coor1[i-1],coor2[i-1],coor1[i],coor2[i]);
+	}
+	linea(coor1[tam-1],coor2[tam-1],coor1[0],coor2[0]);
 }
 
 void mousePtPlot(GLint button , GLint action , GLint xMouse, GLint yMouse)
 {
 	if(button==GLUT_LEFT_BUTTON && action==GLUT_DOWN)
 	{
-		if(bandx==false)
-		{
-			x1=xMouse;
-			y1=winHeight-yMouse;
-			plotPoint(x1,y1);
-			bandx=true;
-
-		}
-		else
-		{
-			x2=xMouse;
-			y2=winHeight-yMouse;
-			plotPoint(x2,y2);
-			bandy=true;
-		}
-		if(bandx==true && bandy==true)
-		{
-			linea(x1,y1,x2,y2);
-			x1=-1;
-			y1=-1;
-			x2=-1;
-			y2=-1;
-			bandx=false;
-			bandy=false;
-		}
+		
+		printf("obtiene coordenada: ");
+		x1=xMouse;
+		y1=winHeight-yMouse;
+		plotPoint(x1,y1);
+		printf("%d %d\n",x1,y1);
+		coor1.push_back(x1);
+		coor2.push_back(y1);
+		
+		
+	}
+	if(button==GLUT_RIGHT_BUTTON && action==GLUT_DOWN)
+	{
+		printf("ultima coordenada: ");
+		x1=xMouse;
+		y1=winHeight-yMouse;
+		plotPoint(x1,y1);
+		coor1.push_back(x1);
+		coor2.push_back(y1);
+		printf("%d %d\n",x1,y1);
+		poligono();
+		
+		
 	}
 	glFlush();
 	
 }
 int main(int argc, char**argv)
 {
+	
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(200,100);
 	
 	glutInitWindowSize(winWidth, winHeight );
-	glutCreateWindow("Mouse PlotPoints" );
+	glutCreateWindow("Poligono con puntos por mouse" );
 	init();
 	glutDisplayFunc(displayFcn);
 	glutReshapeFunc(winReshapeFcn);
 	glutMouseFunc(mousePtPlot);
 	glutMainLoop();
 }
+
